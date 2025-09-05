b3887164183f8d74fe7f17992d05d5f698d469e8
Stuepp <arthurh.c@hotmail.com>
2025-09-05 14:05:10 -0300

    Created display message function

--- app.py
+++ app.py
@@ -9,8 +9,15 @@ st.set_page_config(layout='wide')
 
 # Remote repository updated, now it's diff from local one -- Answears section
 
+def print_msg(msg):
+  print(f'{msg}')
+  return None
+
 def main():
   df_raw = load_data()
+  ping = 'pong'
+
+  print_msg(ping)
 
   # callign data modification section
 
